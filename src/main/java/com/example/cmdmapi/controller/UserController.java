ae31215af7d537641785b1520ab52cae99d60f95
Lucas Melnik <lucas_melnik.99@hotmail.com>
2021-06-02 18:32:51 -0300

    Add Swagger.io

--- src/main/java/com/example/cmdmapi/controller/UserController.java
+++ src/main/java/com/example/cmdmapi/controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.cmdmapi.controller;
 
 import com.example.cmdmapi.model.User;
 import com.example.cmdmapi.repository.UserRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -9,17 +11,20 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@Api(value = "User")
 @RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
+    @ApiOperation(value = "Mostrar todos os usuários cadastrados")
     @GetMapping
     public List<User> listItems() {
         return userRepository.findAll();
     }
 
+    @ApiOperation(value = "Cadastrar um novo usuário")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public User addItem (@RequestBody User user) {
