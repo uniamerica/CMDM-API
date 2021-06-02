66397a9faa1d8f58ee33940e113c58d633a6ef48
Lucas Melnik <lucas_melnik.99@hotmail.com>
2021-06-02 18:42:55 -0300

    add CRUD

--- src/main/java/com/example/cmdmapi/controller/UserController.java
+++ src/main/java/com/example/cmdmapi/controller/UserController.java
@@ -6,6 +6,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,5 +30,26 @@ public class UserController {
     @ResponseStatus(HttpStatus.CREATED)
     public User addItem (@RequestBody User user) {
         return userRepository.save(user);
+    }
+
+    @ApiOperation(value = "Burscar usuário por id")
+    @GetMapping(path = {"/{id}"})
+    public ResponseEntity findById(@PathVariable long id){
+        return userRepository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping(value="/{id}")
+    public ResponseEntity update(@PathVariable("id") long id,
+                                 @RequestBody User user) {
+        return userRepository.findById(id)
+                .map(record -> {
+                    record.setFirstName(user.getFirstName());
+                    record.setEmail(user.getEmail());
+                    record.setPhone(user.getPhone());
+                    User updated = userRepository.save(record);
+                    return ResponseEntity.ok().body(updated);
+                }).orElse(ResponseEntity.notFound().build());
     }
 }
