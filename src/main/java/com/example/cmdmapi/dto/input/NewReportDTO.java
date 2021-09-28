b35873be1529337bcbbfe14332a1ff3f77a9dced
Nelson J S Baez <bluexbaez@gmail.com>
2021-09-27 21:11:41 -0300

    Adicionado usuario ao report e adicionado validações

--- src/main/java/com/example/cmdmapi/dto/input/NewReportDTO.java
+++ src/main/java/com/example/cmdmapi/dto/input/NewReportDTO.java
@@ -1,31 +1,37 @@
 package com.example.cmdmapi.dto.input;
 
+import com.example.cmdmapi.dto.UserDTO;
 import com.example.cmdmapi.model.Report;
+import com.example.cmdmapi.model.User;
+import com.example.cmdmapi.service.UserService;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @RequiredArgsConstructor
 @AllArgsConstructor
 @Builder
 @Data
 public class NewReportDTO {
-
     @NotBlank(message = "Title is Required")
     private String title;
     @NotBlank(message = "Description is Required")
     private String description;
     @NotBlank(message = "Depoiment is Required")
     private String depoiment;
+    @NotNull(message = "User id is required")
+    private Long userId;
 
     public Report toModel(){
         Report report = new Report();
         report.setTitle(getTitle());
         report.setDescription(getDescription());
         report.setDepoiment(getDepoiment());
+        report.setUser(new User(userId));
 
         return report;
     }
