f80b11f5e6cf66a5d0bd280d65de6dc94ee57f19
Lucas Melnik <lucas_melnik.99@hotmail.com>
2021-10-04 21:36:41 -0300

    fixing route problems

--- src/main/java/com/example/cmdmapi/security/SecurityConfig.java
+++ src/main/java/com/example/cmdmapi/security/SecurityConfig.java
@@ -32,7 +32,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/swagger-ui.html",
             "/webjars/**",
             "/v3/api-docs/**",
-            "/swagger-ui/**"
+            "/swagger-ui/**",
+            "/reports"
     };
     private final UserDetailsService userDetailsService;
 
@@ -46,12 +47,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.authorizeRequests().antMatchers(AUTH_WHITELIST).permitAll().antMatchers("/**").authenticated();
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
-        http.authorizeRequests().antMatchers(POST, "**/users/**").permitAll();
-        http.authorizeRequests().antMatchers(GET, "**/swagger-ui.html/**").permitAll();
-        http.authorizeRequests().antMatchers(POST, "**/swagger-ui.html**").permitAll();
+        http.authorizeRequests().antMatchers(POST, "/users").permitAll();
+        http.authorizeRequests().antMatchers(GET, AUTH_WHITELIST).permitAll();
+        http.authorizeRequests().antMatchers(GET, "/reports").permitAll();
 //        http.authorizeRequests().antMatchers(GET, "/users/**").hasAnyAuthority();
 //        http.authorizeRequests().antMatchers(GET, "/clients/**").hasAnyAuthority("ROLE_USER");
         http.authorizeRequests().anyRequest().authenticated();
